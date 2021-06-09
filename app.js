5b06873f4fb4be50b2a17e559f267ade9a2cbcc5
zhih <fufong79570@gmail.com>
2021-06-09 22:02:01 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
 const db = require("./models");
 const app = express();
 const port = 3000;
@@ -8,8 +10,16 @@ const port = 3000;
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use(flash());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  next();
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
