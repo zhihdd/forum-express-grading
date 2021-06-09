977c092d1e789c397783e175446dcd170ded0bd7
zhih <fufong79570@gmail.com>
2021-06-09 21:25:46 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-require('./routes')(app)
+require("./routes")(app);
 
-module.exports = app
+module.exports = app;
