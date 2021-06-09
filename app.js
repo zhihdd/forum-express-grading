aa2acb2b9c646f0e0b83de8ed396291904e24f57
zhih <fufong79570@gmail.com>
2021-06-10 00:44:04 +0800

    Change port to env

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const db = require("./models");
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||3000;
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
