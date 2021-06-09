b20917f02957ea387d482d73abcc2b76d3232b60
zhih <fufong79570@gmail.com>
2021-06-09 21:55:55 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const db = require("./models"); 
+const bodyParser = require("body-parser");
+const db = require("./models");
 const app = express();
 const port = 3000;
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const restController = require("../controllers/restController.js");
 const adminController = require("../controllers/adminController");
+const userController = require("../controllers/userController");
 module.exports = (app) => {
   app.get("/", (req, res) => res.redirect("/restaurants"));
   app.get("/restaurants", restController.getRestaurants);
 
   app.get("/admin", (req, res) => res.redirect("/admin/restaurants"));
   app.get("/admin/restaurants", adminController.getRestaurants);
+
+  app.get("/signup", userController.signUpPage);
+  app.post("/signup", userController.signUp);
 };
