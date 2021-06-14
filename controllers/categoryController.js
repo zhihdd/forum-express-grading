d3dd3f4127bbac7b4a5f1040c56e737e38da8837
zhih <fufong79570@gmail.com>
2021-06-14 17:32:53 +0800

    Add feature of creating new category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,6 +7,28 @@ let categoryController = {
       res.render("admin/categories", { categories });
     });
   },
+
+  postCategory: (req, res) => {
+    const name = req.body.name.trim();
+    if (!name) {
+      req.flash("error_messages", "請填寫類別");
+      return res.redirect("/admin/categories");
+    }
+    return Category.findOne({ where: { name } }).then((category) => {
+      if (category) {
+        req.flash("error_messages", "此類別已存在");
+        return res.redirect("/admin/categories");
+      }
+      return Category.create({
+        name,
+      })
+        .then((category) => {
+          req.flash("success_messages", `成功新增 種類:${category.name}`);
+          return res.redirect("/admin/categories");
+        })
+        .catch((error) => console.log("Error", error));
+    });
+  },
 };
 
 module.exports = categoryController;
