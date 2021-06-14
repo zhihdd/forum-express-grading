b19701ef68c24dd035092111a486824b98487e42
zhih <fufong79570@gmail.com>
2021-06-14 17:13:19 +0800

    Add page of admin categories and feature of showing all  categories

---
+++ controllers/categoryController.js
@@ -0,0 +1,12 @@
+const db = require("../models");
+const Category = db.Category;
+
+let categoryController = {
+  getCategories: (req, res) => {
+    return Category.findAll({ raw: true, nest: true }).then((categories) => {
+      res.render("admin/categories", { categories });
+    });
+  },
+};
+
+module.exports = categoryController;
